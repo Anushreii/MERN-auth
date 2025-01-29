03f7f8147c5f7c5c32d8a7e7b3a3aa577c1dfd31
anushree <anushreev53@gmail.com>
2025-01-29 18:12:23 +0530

    Header

--- task-app/src/App.jsx
+++ task-app/src/App.jsx
@@ -5,15 +5,18 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Profile from './pages/Profile'
+import Header from  './components/Header'
 export default function App(){
   return (
   <BrowserRouter>
+  {/* Header for all pages */}
+  <Header/>
   <Routes>
-    <Route path='/' element={<Home></Home>}></Route> 
-    <Route path='About' element={<About></About>}></Route>
-    <Route path='Signin' element={<Signin></Signin>}></Route>
-    <Route path='Signup' element={<Signup></Signup>}></Route>
-    <Route path='Profile' element={<Profile></Profile>}></Route>
+    <Route path='/' element={<Home/>}></Route> 
+    <Route path='About' element={<About/>}></Route>
+    <Route path='Signin' element={<Signin/>}></Route>
+    <Route path='Signup' element={<Signup/>}></Route>
+    <Route path='Profile' element={<Profile/>}></Route>
   </Routes>
      </BrowserRouter>
   );
---
+++ task-app/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='bg-slate-300'>
+        <div className='flex justify-between items-center max-w-6xl max-auto p-3'>
+            <Link to='/'>
+      <h1 className='font-extrabold'>AUTH APP</h1></Link> 
+      <ul className='flex gap-4'>
+        <Link to='/'><li>Home</li></Link> 
+        <Link to='/About'> <li>About</li></Link> 
+        <Link to='/Signin'> <li>Signin</li></Link> 
+        {/* <Link to='/Singup'><li>Singup</li></Link>  */}
+      </ul>
+    </div>
+    </div>
+  )
+}
+
+export default Header
